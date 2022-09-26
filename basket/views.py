608ad6b1dfb5bb8a5fadddfd8a46c77e9d67009b
shaun-davies <shaundavieswork@outlook.com>
2022-09-26 03:34:23 +0000

    Add adjust and remove from basket views and calc subtotal template filter

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -22,3 +22,32 @@ def add_to_basket(request, item_id):
     request.session['basket'] = basket
     return redirect(redirect_url)
 
+
+def adjust_basket(request, item_id):
+    """ Adjust quantity of selected product to chosen amount """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove item from shopping bag """
+
+    try:
+        basket = request.session.get('basket', {})
+
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponce(status=200)
+
+    except Exception as e:
+        return HttpResponce(status=500)
