3eaa9c8099a3f80fa45b34992ac8dbb1d0bbd4e7
shaun-davies <shaundavieswork@outlook.com>
2022-09-27 00:08:13 +0000

    Add sku field to product model

--- products/models.py
+++ products/models.py
@@ -43,6 +43,7 @@ class Product(models.Model):
     abv = models.DecimalField(max_digits=4, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
