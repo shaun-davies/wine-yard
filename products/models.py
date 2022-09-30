6afe192a64f8fd728cfcccb7632bd0c75fd74e57
shaun-davies <shaundavieswork@outlook.com>
2022-09-30 23:39:23 +0000

    Add image fields to country model

--- products/models.py
+++ products/models.py
@@ -23,6 +23,8 @@ class Country(models.Model):
 
     name = models.CharField(max_length=100)
     friendly_name = models.CharField(max_length=100, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
