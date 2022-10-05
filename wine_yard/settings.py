2310026b533f5e759428b8827c7f0b68df889646
shaun-davies <shaundavieswork@outlook.com>
2022-10-05 19:00:58 +0000

    Deployment

--- wine_yard/settings.py
+++ wine_yard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-35zd^ov&sghchyli^botcwu*8%afy3s)iiogp(rcp2ddh7*w2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wineyard', 'localhost']
 
 
 # Application definition
@@ -107,7 +107,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -119,28 +119,18 @@ WSGI_APPLICATION = 'wine_yard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
-
-#DATABASES = {
-#    'default': dj_database_url.parse('postgres://bwvserloafwtad:fa1f87e14859038e7f4313f1f97c4d9a3da54fd5b7ced7c8118fa9b97f1be5eb@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/dd2h53gmk0q10o')
-#}
-
-#if "DATABASE_URL" in os.environ:
-#    DATABASES = {
-#        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#    }
-#else:
-#    DATABASES = {
-#        "default": {
-#            "ENGINE": "django.db.backends.sqlite3",
-#            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#        }
-#    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
