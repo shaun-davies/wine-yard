bd814d5ab3634a9415011c072ec1d37e8d4726f6
shaun-davies <shaundavieswork@outlook.com>
2022-10-05 20:27:22 +0000

    Remove secret key and set debug

--- wine_yard/settings.py
+++ wine_yard/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-35zd^ov&sghchyli^botcwu*8%afy3s)iiogp(rcp2ddh7*w2@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wineyard.herokuapp.com', 'localhost']
 
